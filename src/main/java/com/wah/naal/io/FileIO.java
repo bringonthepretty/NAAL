bc42d4fbc54846e9acd4bd8986c266149305385f
Kirill Guzarevich <bringonthepretty@gmail.com>
2023-01-26 01:47:03 +0300

    documentation updated

--- src/main/java/com/wah/naal/io/FileIO.java
+++ src/main/java/com/wah/naal/io/FileIO.java
@@ -77,7 +77,7 @@ public class FileIO {
 
     /**
      * Reads all bvh files from provided directory as map where key is file name and value is file data
-     * @return all motions files from application's run directory
+     * @return all bvh files from application's run directory
      */
     public Map<String, String> loadAllBvh(String path) {
         Map<String, String> result = new HashMap<>();
@@ -99,7 +99,7 @@ public class FileIO {
 
     /**
      * Reads all bvh files from application's run directory as string
-     * @return all motions files from application's run directory
+     * @return all bvh files from application's run directory
      */
     public Map<String, String> loadAllBvh() {
         String path = getCurrentPath();
